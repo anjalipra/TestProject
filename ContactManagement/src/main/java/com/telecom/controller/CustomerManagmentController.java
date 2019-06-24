99380800a50b1dafcf24feaf9c2f94ffbc17e7ca
anjali.prakash <anjali.prakash@192.168.1.101>
2019-06-24 11:17:58 +1000

    Fixed issues.

--- ContactManagement/src/main/java/com/telecom/controller/CustomerManagmentController.java
+++ ContactManagement/src/main/java/com/telecom/controller/CustomerManagmentController.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +17,7 @@ import com.telecom.model.ContactInformation;
 import com.telecom.service.ContactService;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CustomerManagmentController {
 	
 	private static final Logger logger = LogManager.getLogger(CustomerManagmentController.class);
